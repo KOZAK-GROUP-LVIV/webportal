e85478fa90e83a8c94004edbebe125c1831ac886
Maxim Karpinka <mak55755@gmail.com>
2017-05-12 23:55:00 +0300

    DB

--- app.js
+++ app.js
@@ -69,7 +69,6 @@ var sessionStore = new MongoStore({
     });
 */
 
-
 var sessionStore = new MongoStore({
       url: 'mongodb://maxim:123987bonus@ds139761.mlab.com:39761/webportalkozakgroup'
     });
