67e0c77e517d52bf5dbdff422ae98be8b6d79a9c
Maxim Karpinka <mak55755@gmail.com>
2017-05-12 22:32:26 +0300

    mongoDB

--- db/config.js
+++ db/config.js
@@ -1,9 +1,16 @@
-
+/*
 module.exports = {
     "port" : 27017,
     "mongoose": {
         "url": "mongodb://localhost/chat"
     }
 }
+*/
 
+module.exports = {
+    "port" : 27017,
+    "mongoose": {
+        "url": "mongodb://maxim:123987bonus@localhost:27017/chat"
+    }
+}
 
