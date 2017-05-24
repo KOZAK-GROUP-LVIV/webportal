59364abd862b74dbd37887d30acb500d34e87048
Maxim Karpinka <mak55755@gmail.com>
2017-05-24 13:04:41 +0300

    domainUrl

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ function onAuthorizeFail(data, message, error, accept){
 
 
 http.listen(3000, function(){
-  //console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
 
 
