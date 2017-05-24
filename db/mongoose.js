142a20268ebe374ab227fc17f7acca0a8d15a24b
Maxim Karpinka <mak55755@gmail.com>
2017-05-24 13:56:45 +0300

    domainUrl

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let config = require("./config.js");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://maxim:123987bonus@ds139761.mlab.com:39761/webportalkozakgroup');
+mongoose.connect(config.mongoose.url);
 
 module.exports = mongoose;
 
